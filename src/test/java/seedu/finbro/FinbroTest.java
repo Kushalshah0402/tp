d537d4e37b508af4c80e677ebe66e95e13244a79
XuZihao <xuzihao024572@gmail.com>
2026-03-17 20:21:33 +0800

    Update ExpenseTest and FinbroTest

--- src/test/java/seedu/finbro/FinbroTest.java
+++ src/test/java/seedu/finbro/FinbroTest.java
@@ -18,7 +18,7 @@ class FinbroTest {
     @Test
     void run_remainingBudgetAboveLimit_showsOverspendingWarning() {
         ExpenseList list = new ExpenseList();
-        list.add(new Expense(180, "entertainment", "1 Jan 2026"));
+        list.add(new Expense(180, "entertainment", "2026-01-01"));
         Limit.initLimit(200);
 
         assertTrue(list.getRemainingExpenditure() <= 20);
@@ -27,7 +27,7 @@ class FinbroTest {
     @Test
     void run_remainingBudgetBelowLimit_doesNotShowOverspendingWarning() {
         ExpenseList list = new ExpenseList();
-        list.add(new Expense(179, "entertainment", "1 Jan 2026"));
+        list.add(new Expense(179, "entertainment", "2026-01-01"));
         Limit.initLimit(200);
 
         assertFalse(list.getRemainingExpenditure() <= 20);
