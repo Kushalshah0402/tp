723fb4583106e2245540ead875872462b3a7682c
Malcolm Tan <malcolmtan2003@gmail.com>
2026-03-20 12:48:44 +0800

    Remove spent attribute from Limit.java

--- src/main/java/seedu/finbro/utils/Limit.java
+++ src/main/java/seedu/finbro/utils/Limit.java
@@ -5,21 +5,11 @@ import java.util.logging.Logger;
 
 public class Limit {
     private static double limit = 0;
-    private static double spent = 0;
 
     private static final Logger logger = Logger.getLogger(Limit.class.getName());
 
     public static double getLimit() {
         return limit;
-    }
-
-    public static double getSpent() {
-        return spent;
-    }
-
-    public static void setSpent(double spent) {
-        logger.log(Level.INFO, "Setting spent to {0}", spent);
-        Limit.spent = spent;
     }
 
     public static String toFileFormat() {
