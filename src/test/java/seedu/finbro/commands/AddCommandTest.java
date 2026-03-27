bedd8cfb354f48169911c2cc6dbe822bb6ee9ec3
Kushalshah0402 <kshah2407@gmail.com>
2026-03-27 12:06:10 +0800

    JUnit tests added for the add expenses command

---
+++ src/test/java/seedu/finbro/commands/AddCommandTest.java
@@ -0,0 +1,72 @@
+package seedu.finbro.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.finbro.storage.Storage;
+import seedu.finbro.ui.Ui;
+import seedu.finbro.utils.ExpenseList;
+
+public class AddCommandTest {
+    //@@author Kushalshah0402
+    @Test
+    void execute_walkthrough_validInput_expenseAdded() throws Exception {
+
+        // Simulated user inputs(each line = Enter key)
+        String simulatedInput =
+                "20\n" +
+                "food\n" +
+                "2026-01-01\n" +
+                "yes\n";
+
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+        Ui ui = new Ui();
+        ExpenseList list = new ExpenseList();
+        Storage storage = null; // not used in walkthrough
+        AddCommand command = new AddCommand("");
+        command.execute(list, ui, storage);
+        assertEquals(1, list.size());
+    }
+
+    //@@author Kushalshah0402
+    @Test
+    void execute_walkthrough_userCancels_noExpenseAdded() throws Exception {
+        String input =
+                "20\n" +
+                "food\n" +
+                "2026-01-01\n" +
+                "no\n";
+
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Ui ui = new Ui();
+        ExpenseList list = new ExpenseList();
+        AddCommand command = new AddCommand("");
+        command.execute(list, ui, null);
+        assertEquals(0, list.size());
+    }
+
+    //@@author Kushalshah0402
+    @Test
+    void execute_strictMode_validInput_expenseAdded() throws Exception {
+        ExpenseList list = new ExpenseList();
+        Ui ui = new Ui();
+        Storage storage = null;
+        AddCommand command = new AddCommand("20 food 2026-01-01");
+        command.execute(list, ui, storage);
+        assertEquals(1, list.size());
+    }
+
+    //@@author Kushalshah0402
+    @Test
+    void execute_strictMode_missingField_throwsException() {
+        ExpenseList list = new ExpenseList();
+        Ui ui = new Ui();
+        Storage storage = null;
+        AddCommand command = new AddCommand("20 2026-01-01"); // missing category
+        assertThrows(Exception.class, () -> command.execute(list, ui, storage));
+    }
+}
