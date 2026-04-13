76ba16d73536e7eef1534b2bbafd54e2246a4dcb
Malcolm Tan <malcolmtan2003@gmail.com>
2026-04-13 13:16:49 +0800

    Add logging for budget warning service

--- src/main/java/seedu/finbro/utils/BudgetWarningService.java
+++ src/main/java/seedu/finbro/utils/BudgetWarningService.java
@@ -1,5 +1,8 @@
 package seedu.finbro.utils;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import seedu.finbro.finances.ExpenseList;
 import seedu.finbro.finances.Limit;
 import seedu.finbro.ui.Ui;
@@ -10,6 +13,8 @@ import seedu.finbro.ui.Ui;
  */
 public class BudgetWarningService {
 
+    private static final Logger logger = Logger.getLogger(BudgetWarningService.class.getName());
+
     /**
      * Checks the current expenditure against the set limit and shows appropriate warnings
      * if the limit is close to being exceeded or has been exceeded.
@@ -19,13 +24,16 @@ public class BudgetWarningService {
      */
     public void checkAndShowWarnings(ExpenseList expenses, Ui ui) {
         double monthlyTotal = expenses.getCurrentMonthTotalExpenditure();
+        logger.log(Level.INFO, "Monthly Total Expenditure: " + monthlyTotal);
 
         if (monthlyTotal - Limit.getLimit() > 0) {
+            logger.log(Level.INFO, "Exceeded monthly budget limit: " + Limit.getLimit());
             ui.showBudgetExceeded(Limit.getLimit());
         } else if (Limit.getLimit() - monthlyTotal <= 20) {
+            logger.log(Level.INFO, "Approaching monthly budget limit: " + Limit.getLimit());
             ui.showBudgetReminder(Limit.getLimit());
         } else {
-            return;
+            logger.log(Level.INFO, "Within monthly budget limit: " + Limit.getLimit());
         }
     }
 }
